5155f02b5b4985a3904412989ea77017eb56b954
Damo <damonkhan92@gmail.com>
2017-08-04 13:22:35 +1200

    Adding socket for connections

--- HttpServer.java
+++ HttpServer.java
@@ -6,21 +6,26 @@ class HttpServer
   public static void main(String args[])
   {
 
-    int portNum = Integer.parseInt(args[0]);
+    /*
+    * TODO uncomment code to
+    * allow users to specify the
+    * port number*/
+
+    int portNum = 8080;
 
     // test001
-    if (args.length != 1)
-    {
-      System.err.println("Usage: <portnum>");
-      return;
-    }
+    //if (args.length != 1)
+    //{
+      //System.err.println("Usage: <portnum>");
+      //return;
+    //}
 
     //test002
-    if (portNum < 1024)
-    {
-      System.err.println("Port number cannot be less than 1024");
-      return;
-    }
+    //if (portNum < 1024)
+    //{
+      //System.err.println("Port number cannot be less than 1024");
+      //return;
+    //}
 
 
 
@@ -28,6 +33,14 @@ class HttpServer
     {
       ServerSocket server = new ServerSocket(portNum);
       System.out.println("web server starting");
+
+      while(true)
+      {
+        System.out.println("Server listening on port " + portNum);
+        Socket client = server.accept(); // wait for client to connect
+        HttpServerSession serverSession = new HttpServerSession(client);
+        System.out.println("Connection received.");
+      }
     }
     catch(IOException e)
     {
@@ -39,6 +52,7 @@ class HttpServer
 class HttpServerSession extends Thread
 {
   private Socket client;
+
 
   public HttpServerSession(Socket client)
   {
